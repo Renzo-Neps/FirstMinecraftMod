d688284e970b3c5b3e096f9576e138b699501d12
Renzo Neps <renzoneps.01@gmail.com>
2023-03-09 21:30:00 -0500

    Initial commit

---
+++ src/main/java/net/renzo/firstmod/FirstModClient.java
@@ -0,0 +1,13 @@
+package net.renzo.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
